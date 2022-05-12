e9d1d15dc0e71bffa2d0578559af18173f043553
hoa0 <t-hoa.l@hotmail.com>
2022-05-12 08:49:45 +0200

    :poop: remove tab, c

--- week-3-sad/script.js
+++ week-3-sad/script.js
@@ -400,52 +400,7 @@ function init ()
 window.onload = init;
 
 
-let btns = document.querySelectorAll(".tab");
-btns.forEach((btn, index) => {
-  // set first button tabindex to 0
-  // and set every other button tabindex to -1
-  if (index == btns.length - 1) {
-    btn.setAttribute("tabindex", 0);
-  } else {
-    btn.setAttribute("tabindex", -1);
-  }
 
-  // add an event listener when C key is pressed
-  btn.addEventListener("keydown", (e) => {
-    if (e.keyCode == 67) { //67 c
-      // prevent the default behaviour
-      e.preventDefault();
-      // set current button tabindex to 0
-      btn.setAttribute("tabindex", -1);
-      // if not last button keep setting tabindex to 0
-      if (btn.previousElementSibling != null) {
-        let nextEl = btn.previousElementSibling;
-        nextEl.setAttribute("tabindex", 0);
-        nextEl.focus();
-      } else {
-        // when we get to last element set first element to tabindex 0
-        // and call focus method on it.
-        // note the .lastElementChild the last element becomes our first 
-        // that's because of the direction we changed
-        
-        let firstEl = document.querySelector("#container")
-          .lastElementChild;
-        firstEl.setAttribute("tabindex", 0);
-        firstEl.focus();
-      }
-    }
-  });
-
-  btn.addEventListener("click", () => {
-    removeActive();
-    btn.classList.add("active");
-  });
-});
-removeActive = (btn) => {
-  btns.forEach((btn) => {
-    btn.classList.remove("active");
-  });
-};
 
 
 
